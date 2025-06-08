810e449fdcbf80f93d1a0208f367997288d3493c
aishajamal4 <aisha14102003@gmail.com>
2025-06-09 02:57:27 +0300

    final code last edition

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -20,31 +20,3 @@ class ThemeManager {
 }
 
 
-
-/* import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-class ThemeProvider extends ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.light;
-
-  ThemeMode get themeMode => _themeMode;
-
-  ThemeProvider() {
-    _loadTheme();
-  }
-
-  void toggleTheme(bool isDarkMode) async {
-    _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isDarkMode', isDarkMode);
-    notifyListeners();
-  }
-
-  Future<void> _loadTheme() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool? isDark = prefs.getBool('isDarkMode');
-    _themeMode = isDark == true ? ThemeMode.dark : ThemeMode.light;
-    notifyListeners();
-  }
-}
- */
